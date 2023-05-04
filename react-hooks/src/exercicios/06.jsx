019e36ad0f6729b5cf27d996eab0a1a6503644b8
KarlaViscondi <90910654+KarlaViscondi@users.noreply.github.com>
2023-05-04 12:09:26 +0000

    (04/05) Início do projeto Karangos

--- react-hooks/src/exercicios/06.jsx
+++ react-hooks/src/exercicios/06.jsx
@@ -8,9 +8,16 @@ import {PokemonForm, fetchPokemon, PokemonInfoFallback, PokemonDataView } from '
 
 function PokemonInfo({pokemonName}) {
   // 🐨 crie o estado para o pokémon (null)
-  const [pokemon, setPokemon] = React.useState(null)
-  const [error, setError] = React.useState(null)
-  const [status, setStatus] = React.useState('idle') //aguardando informações
+  // const [pokemon, setPokemon] = React.useState(null)
+  // const [error, setError] = React.useState(null)
+  // const [status, setStatus] = React.useState('idle') //aguardando informações
+  const [state, setState] = React.useState({
+    pokemon: null,
+    error: null,
+    status: 'idle'
+  })
+  //Criando constantes somente leitura por meio de desestruturação
+  const {pokemon, error, status} = state
 
   // 🐨 crie React.useEffect de modo a ser chamado sempre que pokemonName mudar.
   // 💰 NÃO SE ESQUEÇA DO VETOR DE DEPENDÊNCIAS!
@@ -20,27 +27,31 @@ function PokemonInfo({pokemonName}) {
     if(! pokemonName) return
     // 🐨 antes de chamar `fetchPokemon`, limpe o estado atual do pokemon
     // ajustando-o para null.
-    setPokemon(null)
-    setError(null)
+    // setPokemon(null)
+    // setError(null)
+    setState({pokemon: null, error: null, status: 'pending'})
     // (Isso é para habilitar o estado de carregamento ao alternar entre diferentes
     // pokémon.)
     // 💰 Use a função `fetchPokemon` para buscar um pokémon pelo seu nome:
     //   fetchPokemon('Pikachu').then(
     //     pokemonData => {/* atualize todos os estados aqui */},
     //   )
-    setStatus('pending') //Requisição feita, aguardando desfecho
+    // setStatus('pending') //Requisição feita, aguardando desfecho
     //Requisição da certo - then
     fetchPokemon(pokemonName).then(
       PokemonData => {
-        setPokemon(PokemonData)
-        setStatus('resolved') //promessa cumprida, deu certo
+        // setPokemon(PokemonData)
+        // setStatus('resolved') //promessa cumprida, deu certo
+        setState({ ...state, pokemon: PokemonData, status: 'resolved'})
       } 
     )
     //Requisição deu errado - catch
     .catch(
       error => {
-        setError(error)
-        setStatus('rejected') //promessa frustrada, deu errado
+        // setError(error)
+        // setStatus('rejected') //promessa frustrada, deu errado
+        // setState({ ...state, error: error, status: 'rejected'}) quando os nomes são iguais pode usar um só
+        setState({ ...state, error, status: 'rejected'})
       }
     )
   }, [pokemonName]
@@ -48,6 +59,7 @@ function PokemonInfo({pokemonName}) {
   //useEffect para contagem de atualizações
   React.useEffect(()=>{
     console.count('Atualizou o componente')
+    console.log({state})
   })
   // 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
   // 🐨 retorne o seguinte baseado nos estados `pokemon` e `pokemonName`:
